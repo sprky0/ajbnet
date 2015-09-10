10c4aecbb78dc886831407af3ff9d3608e709238
Avery Brooks <averyjamesbrooks@gmail.com>
2015-09-09 19:00:22 -0500

    bootstrap comment

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,11 @@
 
 define('AJBNET_DIR', realpath(dirname(__FILE__)));
 
+/**
+ * If we don't have an SplClassLoader autoloader class
+ * already defined elsewhere, create one which will be used
+ * as our autoloader internally.
+ */
 if (!class_exists('SplClassLoader')) {
 	include(AJBNET_DIR . '/loader.php');
 }
