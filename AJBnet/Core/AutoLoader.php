13fe6aeed79f46de79fac5dc9d33bc2a33d37dbf
Avery Brooks <averyjamesbrooks@gmail.com>
2015-09-09 23:29:51 -0400

    fix autoloader

--- AJBnet/Core/AutoLoader.php
+++ AJBnet/Core/AutoLoader.php
@@ -4,8 +4,8 @@ namespace AJBnet\Core;
 
 class AutoLoader extends \SplClassLoader {
 
-	public function __construct() {
-		parent::__construct();
+	public function __construct($ns = null, $includePath = null) {
+		parent::__construct($ns, $includePath);
 	}
 
 }
